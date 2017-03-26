81aba04d9ccf2e88a950939f1159cb2f533bbb05
dragan <dragan@DESKTOP-FG26NT5>
2017-03-26 14:32:28 +0200

    Added comic books list view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -90,5 +90,10 @@ namespace ComicBookGallery.Data
             }
             return comicBookToReturn;
         }
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
     }
 }
